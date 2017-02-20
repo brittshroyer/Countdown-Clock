c2dd7b9785ca8da2cd8f756c3040f802d9f36301
brittshroyer <britt.shroyer@gmail.com>
2017-02-20 16:25:59 -0600

    +lifecycle methods, updating time every second

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ class App extends Component {
         <div className="AppTitle">
           Countdown to {this.state.deadline}
         </div>
-        <Clock />
+        <Clock
+          deadline={this.state.deadline}
+        />
         <input
           placeholder='new date'
           onChange={event => this.setState({newDeadline: event.target.value})}
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -11,13 +11,38 @@ class Clock extends Component{
       seconds: 0
     }
   }
+
+  componentWillMount(){
+    this.getTimeUntil(this.props.deadline);
+  }
+  componentDidMount(){
+    setInterval(() => {
+      this.getTimeUntil(this.props.deadline);
+    }, 1000);
+  }
+
+  getTimeUntil(deadline){
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    console.log('time', time);
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor(time/(1000*60*60) % 24);
+    const days = Math.floor(time/(1000*60*60*24));
+    this.setState({
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    })
+  }
+
   render(){
     return(
       <div>
-        <div className="timer Clock-days">{this.state.days}</div>
-        <div className="timer Clock-hours">{this.state.hours}</div>
-        <div className="timer Clock-minutes">{this.state.minutes}</div>
-        <div className="timer Clock-seconds">{this.state.seconds}</div>
+        <div className="timer Clock-days">{this.state.days} days</div>
+        <div className="timer Clock-hours">{this.state.hours} hours</div>
+        <div className="timer Clock-minutes">{this.state.minutes} minutes</div>
+        <div className="timer Clock-seconds">{this.state.seconds} seconds</div>
       </div>
     )
   }
